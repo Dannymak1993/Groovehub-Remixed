d3617a58ae7b40a302fc6f0ff8a951bad66d51de
danny-mak1993 <danny.mak2@gmail.com>
2023-06-14 19:18:41 -0500

    changes to homepage

--- groovehub-remixed/client/src/pages/homepage/index.js
+++ groovehub-remixed/client/src/pages/homepage/index.js
@@ -1,106 +1,31 @@
 import React from 'react';
 import { Grid, Cell } from 'react-foundation';
 import './style.css';
-
 import GalleryCell from './GalleryCell';
-
 import { useQuery } from '@apollo/client';
 import { QUERY_PLAYLIST } from '../../utils/queries.js'
 
-//this is the data for the GalleryCell's
-
-// const country = {
-//   dataGenre: 'country',
-//   className: 'grid-item country',
-//   name: 'Country'
-// }
-// const classical = {
-//   dataGenre: 'classical',
-//   className: 'grid-item classical',
-//   name: 'Classical'
-// }
-// const hiphop = {
-//   dataGenre: 'hiphop',
-//   className: 'grid-item hiphop',
-//   name: 'Hiphop'
-// }
-// const electronic = {
-//   dataGenre: 'electronic',
-//   className: 'grid-item electronic',
-//   name: 'Electronic'
-// }
-// const anime = {
-//   dataGenre: 'anime',
-//   className: 'grid-item anime',
-//   name: 'Anime'
-// }
-// const disney = {
-//   dataGenre: 'disney',
-//   className: 'grid-item disney',
-//   name: 'Disney'
-// }
-// const rock = {
-//   dataGenre: 'rock',
-//   className: 'grid-item rock',
-//   name: 'Rock'
-// }
-// const jazz = {
-//   dataGenre: 'jazz',
-//   className: 'grid-item jazz',
-//   name: 'Jazz'
-// }
-// const korean = {
-//   dataGenre: 'korean',
-//   className: 'grid-item korean',
-//   name: 'K-Pop'
-// }
-
-//this generates the homepage gallery
-
-// const Homepage = () => {
-//   const { loading, error, data } = useQuery(QUERY_PLAYLIST);
-//   const playlistdata = data?.playlists || {};
-//   if (error) {
-//     console.log(error)
-//   }
-//   console.log(playlistdata)
-//   console.log(typeof playlistdata);
-//   return (
-//     <div>
-//       <Grid className="grid">
-//         {playlistdata.map((item, index) => (
-//           <GalleryCell key={index} data-genre={item.genre} />
-//         ))}
-//       </Grid>
-//     </div>
-//   );
-
-// };
-
-// export default Homepage;
 
 const Homepage = () => {
   const { loading, error, data } = useQuery(QUERY_PLAYLIST);
   const playlistdata = data?.playlists || [];
 
-  if (loading) {
-    return <p>Loading playlists...</p>;
-  }
-
-  if (error) {
-    console.error(error);
-    return <p>Error loading playlists.</p>;
-  }
-
-  if (playlistdata.length === 0) {
-    return <p>No playlists available.</p>;
-  }
-
   return (
     <div>
       <Grid className="grid">
         {playlistdata.map((item, index) => (
-          <GalleryCell key={index} data-genre={item.genre} />
+          <Cell
+            key={index}
+            className={`grid-item ${item.genre}`}
+            data-genre={item.genre}
+          >
+            <div className="gallery-name">{item.name}</div>
+            <img
+              src={`../../images/${item.genre}.jpg`}
+              alt={item.name}
+              className="gallery-image"
+            />
+          </Cell>
         ))}
       </Grid>
     </div>
