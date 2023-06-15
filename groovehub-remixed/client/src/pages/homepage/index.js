b1cfc468805cc94cf6942cd80b8ae8814ad9c6f2
danny-mak1993 <danny.mak2@gmail.com>
2023-06-14 20:06:35 -0500

    changes to viewplaylist component

--- groovehub-remixed/client/src/pages/homepage/index.js
+++ groovehub-remixed/client/src/pages/homepage/index.js
@@ -1,27 +1,34 @@
 import React from 'react';
 import { Grid, Cell } from 'react-foundation';
 import './style.css';
-import GalleryCell from './GalleryCell';
 import { useQuery } from '@apollo/client';
 import { QUERY_PLAYLIST } from '../../utils/queries.js'
 
+function viewPlaylist() {
+
+  window.location.href = 'http://localhost:3000/viewplaylist';
+
+};
 
 const Homepage = () => {
   const { loading, error, data } = useQuery(QUERY_PLAYLIST);
   const playlistdata = data?.playlists || [];
+
+console.log(playlistdata)
 
   return (
     <div>
       <Grid className="grid">
         {playlistdata.map((item, index) => (
           <Cell
+            onClick={() => viewPlaylist(item.spotifyPlaylistID, item.genre)}
             key={index}
             className={`grid-item ${item.genre}`}
             data-genre={item.genre}
           >
             <div className="gallery-name">{item.name}</div>
             <img
-              src={`../../images/${item.genre}.jpg`}
+              src={`/images/${item.genre}.jpg`}
               alt={item.name}
               className="gallery-image"
             />
