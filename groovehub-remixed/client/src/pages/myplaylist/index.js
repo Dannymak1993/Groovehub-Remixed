33e62a258b0b1068f709c9ae610eed52aa117088
danny-mak1993 <danny.mak2@gmail.com>
2023-06-16 10:45:34 -0500

    added new playlist feature

--- groovehub-remixed/client/src/pages/myplaylist/index.js
+++ groovehub-remixed/client/src/pages/myplaylist/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Grid, Cell } from 'react-foundation';
 import './style.css';
-import { useQuery } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 import { useNavigate } from 'react-router-dom';
 import { QUERY_USER_PLAYLIST } from '../../utils/queries.js';
+import { DELETE_USER_PLAYLIST } from '../../utils/mutations.js'
 import GetMyPlaylist from './GetMyPlaylist';
 
 const MyPlaylist = ({ setplaylistInfo }) => {
@@ -11,28 +12,78 @@ const MyPlaylist = ({ setplaylistInfo }) => {
     const Navigate = useNavigate();
     const { loading, error, data } = useQuery(QUERY_USER_PLAYLIST);
     const userPlaylists = data?.userPlaylists || [];
+    // const [deleteUserPlaylist] = useMutation(DELETE_USER_PLAYLIST);
+
+    const handleCreatePlaylist = () => {
+        Navigate('/editplaylist');
+    };
 
     const handlesubmit = (spotifyPlaylistID, name, genre) => {
         setplaylistInfo({ playlist: spotifyPlaylistID, name: name });
         Navigate('/viewplaylist');
     };
+
+    const handleEditPlaylist = (playlistId) => {
+        Navigate(`/editplaylist/${playlistId}`);
+    };
+
+    // const handleDeletePlaylist = async (playlistId) => {
+    //     try {
+    //         await deleteUserPlaylist({
+    //             variables: { playlistId },
+    //             refetchQueries: [{ query: QUERY_USER_PLAYLIST }]
+    //         });
+    //         // Perform any additional actions after deletion if needed
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // };
+
 //this is using the GetMyPlaylist component that calls the api and renders the list of playlists.
     return (
         <div>
 {/* <button onClick={<}>Get My Playlists</button> */}
             {/* <GetMyPlaylist/> */}
+
             <Grid className="grid">
-                
                 {userPlaylists.map((playlist, index) => (
                     <Cell
-                        onClick={() => handlesubmit(playlist.spotifyPlaylistID, playlist.name, playlist.genre)}
                         key={index}
                         className={`grid-item ${playlist.genre}`}
                         data-genre={playlist.genre}
+                        onClick={() =>
+                            handlesubmit(
+                                playlist.spotifyPlaylistID,
+                                playlist.name,
+                                playlist.genre
+                            )
+                        }
                     >
-                        <div className="gallery-name">{playlist.name}</div>
+                        <div className="gallery-content">
+                            <button
+                                className="edit-button"
+                                onClick={() => handleEditPlaylist(playlist._id)}
+                            >
+                                Edit
+                            </button>
+                            <button
+                                className="delete-button"
+                                // onClick={() => handleDeletePlaylist(playlist._id)}
+                            >
+                                Delete
+                            </button>
+                            <div className="gallery-name">{playlist.name}</div>
+                        </div>
                     </Cell>
                 ))}
+                <Cell
+                    className="grid-item create-cell"
+                    onClick={handleCreatePlaylist}
+                >
+                    <div className="create-content">
+                        <span className="plus-sign">+</span>
+                    </div>
+                </Cell>
             </Grid>
         </div>
     );
