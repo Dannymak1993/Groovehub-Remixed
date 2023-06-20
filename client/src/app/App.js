8e512e25dccb7230fdf4ae5555ac9901581a86a3
danny-mak1993 <danny.mak2@gmail.com>
2023-06-20 14:31:54 -0500

    changed app.js

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -14,7 +14,7 @@ import UserContext from '../utils/UserContext';
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql',
+  uri: '/graphql',
 });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` heade
